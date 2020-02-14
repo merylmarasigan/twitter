8a668119f7811100cafcd4a8aede22fca6708bf5
Meryl Marasigan <meryl0729@gmail.com>
2020-02-13 18:03:59 -0800

    finished watching favoriting video but still trying to fix favoriting

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -17,13 +17,34 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var retweetButton: UIButton!
     @IBOutlet weak var favButton: UIButton!
     
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    
     @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }else{
+           TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("unFavorite did not succeed: \(error)")
+            })
+        }
+        
+        
+        
     }
     
     @IBAction func retweet(_ sender: Any) {
     }
-    var favorited:Bool = false
-    
+
     func setFavorite(_ isFavorited:Bool){
         favorited = isFavorited
         if (favorited){
